8a215b3ec943494947b3ec943e78db2410575cec
Monica Marziano <mmarziano1@gmail.com>
2019-02-16 11:12:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,6 +69,22 @@ class Dog
     
   end 
   
+  def self.new_from_db 
+    
+  end 
+  
+  def self.find_by_name(name) 
+    sql = <<-SQL 
+      SELECT * FROM dogs
+      WHERE name = ? 
+    SQL
+    
+    DB[:conn].execute(sql, name)
+  end 
+  
+  def self.update 
+    
+  end 
     
     
   
